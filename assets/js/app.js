63eedfe91007352f563b7baf69f3ee5810c5305a
Olatunde Sleek <olatundesleek@gmail.com>
2020-08-27 11:31:29 +0100

    made changes to the course li

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,22 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   mobileMenu.classList.toggle("showmenulist");
 });
+
+let overviewTab = document.querySelector(".page-tab-11");
+let curriculumTab = document.querySelector(".page-tab-12");
+let overviewContent = document.querySelector(".overview");
+let curriculumContent = document.querySelector(".curriculum");
+
+curriculumTab.addEventListener("click", () => {
+  curriculumTab.classList.add("active-tab");
+  overviewTab.classList.remove("active-tab");
+  overviewContent.style.display = "none";
+  curriculumContent.style.display = "block";
+});
+
+overviewTab.addEventListener("click", () => {
+  curriculumTab.classList.remove("active-tab");
+  overviewTab.classList.add("active-tab");
+  overviewContent.style.display = "block";
+  curriculumContent.style.display = "none";
+});
